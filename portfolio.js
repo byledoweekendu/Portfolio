3ad08d9cb96f39a9b4b72f212adf6760843e8edd
Jacek Szymański <123887799+jacekszymanski95@users.noreply.github.com>
2024-02-02 10:40:22 +0100

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -6,22 +6,20 @@ const home = `<span id="hi" class="text">Cześć, nazywam się</span>
 
 const about = `<span id ="description" class="text">Początkujący frontend developer po kursie Java Script + React od podstaw.</br></br>
 Zajmuję się projektowaniem stron internetowych, a konkretnie ich wyglądem i zachowaniem po interakcji użytownika z jej elementami.</br></br> 
-Tworzę też wizytówki google i zarządzam nimi.</br></br> 
 Zapraszam do zapoznania się z moimi projektami.</br></br> 
-Podzieliłem je na responsywne i nieresponsywne.</br></br>
-Responsywne to te, które dostosowane są do wyświetlania zarówno na dużych ekranach komputerów jak i małych mobilnych.</br></br>
-Nieresponsywne to projekty tylko na duże ekrany.</br></br>
-Projekty tworzę w zależności od potrzeb.</br></br>
-Stworzę stronę firmy, portfolio lub pomogę przy projektach.</br></br>
-Zaprojektuję też i będę aktualizować dowolną wizytówkę google.</span>`;
+Stworzę stronę wizytówkę, portfolio lub projekt indywidualny dla firmy.</br></br>
+Zoptymalizuję też i będę moderować dowolną wizytówkę google.</br></br>
+Jestem otwarty na wszelką współpracę, udział w projektach oraz zlecenia, staże i prace jako Front End Developer.</br></br>
+Zapraszam serdecznie do kontaktu.   
+</span>`;
 
 const card = `<span id ="cardDescr" class="text">Wizytówki google, to nic innego jak wszystkie te pozycje, które wyświetlają się w google po wpisaniu danej frazy np. "Elektryk".</br></br>
 Po wpisaniu takiej frazy i miejsca np. "Poznań" wyskoczą wszyscy elektrycy z Poznania i okolic, którzy mają taką właśnie wizytówkę.</br></br> 
 Taka wizytówka to główny kanał napływu klientów dla firmy.</br></br> 
-Dzięki temu, że w takiej wizytówce dodamy dane firmy, jej opis, specjalizacje, zdjęcia, godziny otwarcia i wiele innych informacji oraz odpowiemy na posty klientów,
-mamy realny wpływ na to jak wysoko nasza firma jest w wynikach wyszukiwania, a w ten sposób zwiększamy sobie napływ klientów.</br></br> 
-Im więcej informacji, postów i szczegołów tym wyżej jest nasza firma i tym więcej będzie miała pozytywnych recenzji i klientów.</br></br>
-Drugim kanałem napływu klientów jest strona wizytówka na facebooku.</br></br>
+Dzięki temu, że w takiej wizytówce dodamy wszystkie szczegółowe dane firmy, jej opis, specjalizacje, zdjęcia, godziny otwarcia i wiele innych informacji oraz odpowiemy na posty klientów,
+mamy realny wpływ na to jak wysoko nasza wizytówka jest w wynikach wyszukiwania, a w ten sposób zwiększamy sobie napływ klientów do firmy.</br></br> 
+Im więcej informacji, postów, szczegołów oraz aktywności w wizytówce tym wyżej jest nasza firma i tym więcej będzie miała pozytywnych recenzji i klientów.</br></br>
+Drugim kanałem napływu klientów jest funpage na facebooku.</br></br>
 Oba te kanały łączymy dedykowaną stroną internetową, która pozwala przedstawić firmę w 100% i idealnie spina wszystko w całość.</br></br>
 Oczywiście nie każdy biznes potrzebuje dedykowaną stronę, ale w wielu przypadkach warto ją mieć, bo nie wszystko uda się dobrze przedstawić na google czy facebooku.</br></br>
 </span>`
@@ -33,20 +31,21 @@ const skills = `<section id="br1"><img src="img portfolio/html.png" class="icon"
 <section id="br5"><img src="img portfolio/react.png" class="icon" alt="React"/><span class="text3">React</span></section>
 <section id="br6"><img src="img portfolio/eng.png" class="icon" alt="Angielski"/><span class="text3">Angielski - komunikatywny</span></section>`;
 
-const projects = `<span class="text2">Responsywne:</span>
+const projects = `<span class="text2">Strony/Programy:</span>
 <div class="projects">
-    <a href="reminder.html" target="_blank" class="projectTile"><span class="projectName">Przypominacz</span><img src="img portfolio/reminder.jpg" class="projectImg" alt="Przypominacz"/></a>
-    <a href="sklep.html" target="_blank" class="projectTile"><span class="projectName">Sklep</span><img src="img portfolio/sklep.jpg" class="projectImg" alt="Sklep"/></a>
+	<a href="reminder.html" target="_blank" class="projectTile"><span class="projectName">Przypominacz</span><img src="img portfolio/reminder.jpg" class="projectImg" alt="Przypominacz"/></a>
+	<a href="sklep.html" target="_blank" class="projectTile"><span class="projectName">Sklep</span><img src="img portfolio/sklep.jpg" class="projectImg" alt="Sklep"/></a>
 	<a href="przepisy.html" target="_blank" class="projectTile"><span class="projectName">Przepisy</span><img src="img portfolio/przepisy.jpg" class="projectImg" alt="Przepisy"/></a>
-    <a href="portfolioalfa.html" target="_blank" class="projectTile"><span class="projectName">Alfa Portfolio</span><img src="img portfolio/alfa.jpg" class="projectImg" alt="Alfa Portfolio"/></a>
-    <a href="portfoliobeta.html" target="_blank" class="projectTile"><span class="projectName">Beta Portfolio</span><img src="img portfolio/beta.jpg" class="projectImg" alt="Beta Portfolio"/></a>
-	<a href="firma.html" target="_blank" class="projectTile"><span class="projectName">DREW-LIS</span><img src="img portfolio/drew-lis.jpg" class="projectImg" alt="Drew-Lis"/></a>
-</div>
-<span class="text2">Nieresponsywne:</span>
-<div class="projects">
     <a href="kalkulator.html" target="_blank" class="projectTile"><span class="projectName">Kalkulator wydatków</span><img src="img portfolio/kalkulator.jpg" class="projectImg" alt="Kalkulator"/></a>
     <a href="quiz.html" target="_blank" class="projectTile"><span class="projectName">Quiz tabliczka mnożenia</span><img src="img portfolio/quiz.jpg" class="projectImg" alt="Quiz"/></a>
     <a href="gra.html" target="_blank" class="projectTile"><span class="projectName">Gra w zgadywanie</span><img src="img portfolio/gra.jpg" class="projectImg" alt="Gra"/></a>
+</div>
+<span class="text2">Wizytówki:</span>
+<div class="projects">
+	<a href="firma.html" target="_blank" class="projectTile"><span class="projectName">DREW-LIS</span><img src="img portfolio/drew-lis.jpg" class="projectImg" alt="Drew-Lis"/></a>
+	<a href="rostlog.html" target="_blank" class="projectTile"><span class="projectName">RostLog S.C</span><img src="img portfolio/rostlog.jpg" class="projectImg" alt="RostLog"/></a>
+    <a href="portfolioalfa.html" target="_blank" class="projectTile"><span class="projectName">Alfa Portfolio</span><img src="img portfolio/alfa.jpg" class="projectImg" alt="Alfa Portfolio"/></a>
+    <a href="portfoliobeta.html" target="_blank" class="projectTile"><span class="projectName">Beta Portfolio</span><img src="img portfolio/beta.jpg" class="projectImg" alt="Beta Portfolio"/></a>
 </div>`;
 
 const prices = `<span class="text4">Średni koszt strony w 2024:</span>
@@ -57,10 +56,10 @@ Cena projektu i wdrożenia strony internetowej może znacznie wzrosnąć, jeśli
 <span class="text4">U mnie strona kosztuje(zależnie od stopnia skomplikowania) od 1000zł do 2000zł, natomiast najprostsza wizytówkowa od 500zł do 750zł.</br></br>
 Do kosztów strony należy wliczyć też hosting(do 300zł) oraz domenę i certyfikat SSL(do 200zł) na rok.</br></br></br>
 Wizytówka google jest darmowa, google nie pobiera opłat za założenie i utrzymanie.</br></br>
-Opłata jest natomiast za zlecenie założenia i zarządzania taką wizytówką.</span>
+Opłata jest natomiast za zlecenie optymalizacji i zarządzania taką wizytówką.</span>
 <span class="text5">„Działania obejmujące optymalizację i promocję firmy w Google należą do bardziej skomplikowanych, 
 dlatego warto rozważyć powierzenie ich fachowcom. Koszt pozycjonowania wizytówki Google Maps zazwyczaj mieści się w przedziale 500-1000 zł.”</span>
-<span class="text4">U mnie koszt założenia i optymalizacji takiej wizytówki to 100zł, a koszt zarządzania 150zł na miesiąc.</br></br>
+<span class="text4">U mnie koszt optymalizacji takiej wizytówki to 100zł, a koszt zarządzania 150zł na miesiąc.</br></br>
 Czas współpracy przy wizytówce jest dowolny, działam tak długo jak tylko będzie potrzeba.</span>`
 
 const contact = `<form action="https://formsubmit.co/jacekszymanskifronenddeveloper@gmail.com" method="POST">
